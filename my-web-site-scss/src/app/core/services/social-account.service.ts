1054261cdbbd01a1e1f7b2648d65d81e4e46f22c
serkan.ertan88@gmail.com <serkan.ertan88@gmail.com>
2018-08-06 21:38:12 +0200

    social service was added

---
+++ my-web-site-scss/src/app/core/services/social-account.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SocialAccountService {
+
+  constructor(private http: HttpClient) { }
+
+  getSocialAccounts(): Observable<any[]> {
+    return this.http.get<any[]>('./api/social-links-data-json');
+  }
+}
